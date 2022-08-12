1a10657bc3879fb1a4cba8d8a97ccd08be6ce5bc
Levi <43946913+cjl-github@users.noreply.github.com>
2022-08-13 00:30:18 +0800

    Update checkout.groovy

--- src/org/devops/checkout.groovy
+++ src/org/devops/checkout.groovy
@@ -1,10 +1,9 @@
 package org.devops
 
+//下载代码
 def GetCode(branchName, srcUrl){
   checkout([$class: 'GitSCM', 
             branches: [[name: branchName]], 
             extensions: [], 
-            userRemoteConfigs: [[credentialsId: 'gtihub', 
-                                 url: srcUrl]]]) 
-          
+            userRemoteConfigs: [[credentialsId: 'gtihub', url: srcUrl]]]) 
 }
