e8f82b18d094840106cc328c07e5cbcd021e0f39
Levi <43946913+cjl-github@users.noreply.github.com>
2022-08-29 10:18:12 +0800

    Delete checkout.groovy

--- src/org/devops/checkout.groovy
+++
@@ -1,9 +0,0 @@
-package org.devops
-
-//下载代码
-def GetCode(branchName, srcUrl){
-  checkout([$class: 'GitSCM', 
-            branches: [[name: branchName]], 
-            extensions: [], 
-            userRemoteConfigs: [[credentialsId: 'gtihub', url: srcUrl]]]) 
-}
